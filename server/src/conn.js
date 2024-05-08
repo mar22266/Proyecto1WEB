35fc87fea39b9c63c0f65310f125b3b5c2b8376c
Andre Marroquin <mar22266@uvg.edu.gt>
2024-05-08 08:41:55 -0600

    Add

--- server/src/conn.js
+++ server/src/conn.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  port: 3306  // Use the internal MySQL port (default: 3306)
+  port: 3306  
 });
 
 export default pool;
