991ef674db33e2b8a0648f1e90f72610d3184889
Andre Marroquin <mar22266@uvg.edu.gt>
2024-05-07 21:25:57 -0600

    Feat: try

--- client/src/pages/Blog/Blog.jsx
+++ client/src/pages/Blog/Blog.jsx
@@ -10,7 +10,7 @@ const Blog = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('https://ghostdomain.xyz:3001/posts/')
+      const response = await fetch('http://ec2-34-203-120-96.compute-1.amazonaws.com/posts/')
       const data = await response.json()
       setPosts(data)
       setIsLoading(false)
